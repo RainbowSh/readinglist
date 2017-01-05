500c3cff3dcca9bb789265572c5b0b2589e9eeb4
rainbow@sh.cn <aoyi1976@gmail.com>
2017-01-05 17:53:58 +0800

    create project

---
+++ readinglist/src/main/java/com/example/ReadingListRepository.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by Rainbow on 2017/1/5.
+ */
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+}
