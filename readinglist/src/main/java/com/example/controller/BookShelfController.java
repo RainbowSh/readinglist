d6ada939b63ebb90f4aa0ff52d5c2904689f7321
Rainbow@sh.cn <aoyi1976@gmail.com>
2017-01-08 00:38:04 +0800

    add isbn validation

--- readinglist/src/main/java/com/example/controller/BookShelfController.java
+++ readinglist/src/main/java/com/example/controller/BookShelfController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import com.example.model.Book;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.commons.validator.routines.ISBNValidator;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,10 @@ public class BookShelfController {
     @RequestMapping(value = "/{isbn}", method = RequestMethod.GET)
     public Book queryByISBN(@PathVariable("isbn") String isbn) {
         Book book = new Book();
+
+        if (!ISBNValidator.getInstance().isValid(isbn)){
+//            throw new InvalidFormatException();
+        }
 
         book.setAuthor("aoyi");
         book.setReader("aoyi");
